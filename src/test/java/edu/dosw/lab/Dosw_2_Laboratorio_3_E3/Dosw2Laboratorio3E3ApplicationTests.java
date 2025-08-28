655b8cfec9ebd170c583a55c6f31c496254b4a1e
Sofia <Sofia>
2025-08-27 20:00:32 -0500

    initial commit

---
+++ src/test/java/edu/dosw/lab/Dosw_2_Laboratorio_3_E3/Dosw2Laboratorio3E3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Dosw_2_Laboratorio_3_E3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dosw2Laboratorio3E3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
