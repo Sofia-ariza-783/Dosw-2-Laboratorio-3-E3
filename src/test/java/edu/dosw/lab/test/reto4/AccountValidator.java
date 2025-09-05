45a63504d9452e276c235ed9ffb0fdd509c9e773
Sofia Ariza <sofia.ariza-g@mail.escuelaing.edu.co>
2025-09-04 20:21:08 -0500

    Parte 4- Reto 4: Initial Tests

--- src/test/java/edu/dosw/lab/test/reto4/AccountValidator.java
+++ src/test/java/edu/dosw/lab/test/reto4/AccountValidator.java
@@ -1,4 +1,13 @@
 package edu.dosw.lab.test.reto4;
 
+import java.util.Arrays;
+
 public class AccountValidator {
+    public static final String[] AVAILABLE_BANKS = {Bank.BANCOLOMBIA, Bank.DAVIVIENDA};
+    public static boolean isValidAccount(String account) {
+        if (account.length() != 10) {
+            return false;
+        }
+        return Arrays.stream(AVAILABLE_BANKS).noneMatch(account::startsWith);
+    }
 }
--- src/test/java/edu/dosw/lab/test/reto4/Bank.java
+++ src/test/java/edu/dosw/lab/test/reto4/Bank.java
@@ -1,6 +1,6 @@
 package edu.dosw.lab.test.reto4;
 
 public class Bank {
-    String BANCOLOMBIA = "01";
-    String DAVIVIENDA = "02";
+    static String BANCOLOMBIA = "01";
+    static String DAVIVIENDA = "02";
 }
